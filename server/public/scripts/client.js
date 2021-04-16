9cd1915615b78947be0da655d846926e2524fcf6
Karsten Jepsen <Kjepsen86@gmail.com>
2021-04-16 14:31:25 -0500

    Added total guesses append, restart button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,23 +3,50 @@ $(document).ready(handleReady);
 function handleReady() {
   console.log("jquery is loaded!");
   $('#submit-button').on('click', function (event) {
-      console.log('clicked');
+      console.log('clicked submit');
       addInputs();
   });
-  // randomNumber();
+  
+  $('#guess-history').on('click', '#restart-button', function (event) {
+    console.log('clicked restart');
+    clearInputs();
+});
 }
 
-let round = 1;
+function clearInputs() {
+  let resetInput = {
+    roundNumber: 0,
+    player1: 0,
+    player1correct: 2,
+    player2: 0,
+    player2correct: 2,
+  } 
+  $.ajax({
+    method: 'POST',
+    url: '/restart',
+    data: resetInput,
+})
+    .then(function (response) {
+      console.log('added guesses');
+      getGuesses();
+    })
+    .catch( function (error) {
+      console.log('error from server', error);
+      alert('sorry, could not get guesses. Try again later.');
+    })
+  $('#input1').val('');
+  $('#input2').val('');
+}
 
 function addInputs() {
   let newInputs = {
-    roundNumber: round,
+    roundNumber: 0,
     player1: $('#input1').val(),
     player1correct: 0,
     player2: $('#input2').val(),
     player2correct: 0,
   }
-  console.log('adding inputs', newInputs);
+  console.log('Restarting Server', newInputs);
   
   $.ajax({
     method: 'POST',
@@ -36,7 +63,6 @@ function addInputs() {
     })
   $('#input1').val('');
   $('#input2').val('');
-  round++;
 }
 
 function getGuesses() {
@@ -56,16 +82,29 @@ function getGuesses() {
 }
 
 function render( checkedguesses ) {
+  console.log(checkedguesses);
+  
+  $("#round-total").text(`${checkedguesses.length}`);
   $('#total-guesses').empty();
-  $('#guess-history').empty();
-    for (let round of checkedguesses){
-      console.log(`${round.player1}, ${round.player2}`);
-      $('#total-guesses').append(`
-        <div class="guessgame">
-        <p>Round ${round.roundNumber}:</p>
-        <p>Player 1: ${round.player1}</p>
-        <p>Player 2: ${round.player2}</p>
-        </div>
-      `)
+  let string = '';
+    for (let guess of checkedguesses){
+      console.log(`${guess.player1}, ${guess.player2}`);
+      if (guess.player1correct === 1) {
+        string = `<p>Player 1: ${guess.player1} , Too high!</p>`;
+      } else if (guess.player1correct === -1) {
+        string = `<p>Player 1: ${guess.player1} , Too low!</p>`;
+      } else {
+        string = `<p class="correct">Player 1: ${guess.player1} , Right on!</p> <button id="restart-button">Restart</button>`;
+      }
+
+      if (guess.player2correct === 1) {
+        string += `<p>Player 2: ${guess.player2} , Too high!</p>`;
+      } else if (guess.player2correct === -1) {
+        string += `<p>Player 2: ${guess.player2} , Too low!</p>`;
+      } else {
+        string += `<p class="correct">Player 2: ${guess.player2} , Right on!</p> <button id="restart-button">Restart</button>`;
+      }
+
+      $('#total-guesses').append(string);
     }
 }
--- server/server.js
+++ server/server.js
@@ -23,8 +23,17 @@ app.get( '/guesses', (req, res)  => {
 // POST
 app.post('/guesses', (req, res) => {
   let newInputs = req.body;
-  // console.log('got a new Guess, serverside ... ', newGuess);
+  // console.log('Checking format of newInputs', newInputs);
   guesses.push(newInputs);
+  editGuesses();
+  res.sendStatus(201);
+  console.log('got a new Guess, serverside ... ', newInputs);
+})
+
+app.post('/restart', (req, res) => {
+  guesses = [];
+  randomNum = randomNumber();
+  console.log('Reinitializing guesses');
   res.sendStatus(201);
 })
 
@@ -36,3 +45,27 @@ app.listen(PORT, () => {
 function randomNumber() {
   return Math.floor((Math.random()* 25)+1);
  };
+
+ let randomNum = randomNumber();
+
+ function editGuesses() {
+  let guess = guesses[guesses.length - 1];
+  guess.roundNumber = guesses.length;
+  if ( guess.player1 > randomNum ) {
+    guess.player1correct = 1;
+  } else if ( guess.player1 < randomNum ) {
+    guess.player1correct = -1;
+  } else {
+    guess.player1correct = 0;
+  }
+
+  if ( guess.player2 > randomNum ) {
+    guess.player2correct = 1;
+  } else if ( guess.player2 < randomNum ) {
+    guess.player2correct = -1;
+  } else {
+    guess.player2correct = 0;
+  }
+}
+
+
